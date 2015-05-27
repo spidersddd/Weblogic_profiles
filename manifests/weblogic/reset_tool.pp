339743772dcd0db1e52b473852a744d1e07172c4
Troy Klein <troy.klein@puppetlabs.com>
2015-05-27 03:57:01 -0500

    fix reset_tool

--- manifests/weblogic/reset_tool.pp
+++ manifests/weblogic/reset_tool.pp
@@ -1,11 +1,11 @@
 class profile::weblogic::reset_tool {
   file { '/root/reset_host':
     ensure  => present,
-    content => 'puppet:///modules/profile/weblogic/reset_host',
+    source  => 'puppet:///modules/profile/weblogic/reset_host',
     mode    => '0755',
   }
   file { '/root/reset.pp':
     ensure  => present,
-    content => 'puppet:///modules/profile/weblogic/reset.pp',
+    source  => 'puppet:///modules/profile/weblogic/reset.pp',
   }
 }
